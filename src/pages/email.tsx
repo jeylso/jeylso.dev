495963df9db484db9e5ad82e916b7f066b4e0579
José L Silva <jose@silva.app>
2021-02-14 16:40:56 -0500

    Fixed loading environment variable

--- src/pages/email.tsx
+++ src/pages/email.tsx
@@ -1,13 +1,18 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function EmailPage() {
+export default function EmailPage({ email }) {
   const router = useRouter();
 
   useEffect(() => {
-    window.location.href = `mailto:${process.env.EMAIL}`;
+    window.location.href = `mailto:${email}`;
     router.push("/", undefined, { shallow: true });
   }, []);
 
   return <h1 className="hidden">Email</h1>;
+}
+
+export function getStaticProps() {
+  const email = process.env.EMAIL;
+  return { props: { email } };
 }
