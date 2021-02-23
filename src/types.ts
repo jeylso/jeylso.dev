80b1df9776b8f91eed5ee86cb2ffb860f53a3db3
José L Silva <jose@silva.app>
2021-02-23 10:04:08 -0500

    Added type for layouts

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
 
-export type Children = {
+export type LinesLayouts = {
     children: ReactNode;
 };
 
 export type LinkSocials = {
     social: string;
+}
+
+export type Layouts = {
+    children: ReactNode;
+    pageTitle?: string;
+    pageDescription?: string;
+    contentClass?: string | null;
 }
