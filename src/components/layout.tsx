f990de2fe043eec0e3125e70c3326f55b4374657
José L Silva <jose@silva.app>
2021-02-26 14:48:09 -0500

    Added tags for the favicon and manifest

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,6 +18,80 @@ export default function Layout({ children, className }: Layouts) {
         <link rel="canonical" href={`${data.url}/`} />
         <meta name="robots" content="index, nofollow" />
         <meta name="description" content={data.desciption} />
+        {/* Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#333" />
+        {/* Favicons */}
+        <link rel="icon" href="favicon.ico" type="image/x-icon" sizes="16x16" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicons/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicons/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicons/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicons/96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/16x16.png"
+        />
         {/* Twitter */}
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@jeylso" />
